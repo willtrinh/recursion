070d5984ddde48f020f7c565c92e709b01193e89
Will Trinh <williamltrinh@gmail.com>
2021-02-22 15:53:14 -0800

    Assignment Complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,3 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // obj types and conditions check
   if (typeof obj === 'string') {
